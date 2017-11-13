3dff727596fea25c64356d13b29e963cdcef0325
Eunice Jun <emjun@D-69-91-144-40.dhcp4.washington.edu>
2017-11-13 10:43:37 -0800

    Switched to JQuery to implement block drag/drop and reordering on create page. Also now trying to add heart likes on explore page.

--- digestif/static/digestif/create.js
+++ digestif/static/digestif/create.js
@@ -1,74 +1,21 @@
-// target elements with the "draggable" class
-interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
-    restrict: {
-      restriction: "parent",
-      endOnly: true,
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-    // enable autoScroll
-    autoScroll: true,
+$(function () {
+    $(".column").sortable({
+      connectWith: ".column",
+      // handle: ".portlet-header",
+      cancel: ".portlet-toggle",
+      placeholder: "portlet-placeholder ui-corner-all"
+      });
 
-    // call this function on every dragmove event
-    onmove: function dragMoveListener (event) {
-      console.log(event.target)
-      var target = event.target,
-        // keep the dragged position in the data-x/data-y attributes
-        x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-        y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+      $(".portlet")
+        .addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+        .find(".portlet-header")
+          .addClass("ui-widget-header ui-corner-all")
+          .prepend("<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
 
-      // translate the element
-      target.style.webkitTransform =
-      target.style.transform =
-        'translate(' + x + 'px, ' + y + 'px)';
+      $(".portlet-toggle").on("click", function() {
+          var icon = $ (this);
+          icon.toggleClass("ui-icon-minusthick ui-icon-plusthick");
+          icon.closest(".portlet").find(".portlet-content").toggle();
+        });
 
-      // update the posiion attributes
-      target.setAttribute('data-x', x);
-      target.setAttribute('data-y', y);
-    }
-    // // call this function on every dragend event
-    // onend: function (event) {
-    //   var textEl = event.target.querySelector('p');
-    //
-    //   textEl && (textEl.textContent =
-    //     'moved a distance of '
-    //     + (Math.sqrt(event.dx * event.dx +
-    //                  event.dy * event.dy)|0) + 'px');
-    // }
   });
-
-// enable draggables to be dropped into this
-interact('.dropzone').dropzone({
-  // only accept elements matching this CSS selector
-  accept: '#yes-drop',
-  // Require a 75% element overlap for a drop to be possible
-  overlap: 0.75,
-
-  // listen for drop related events:
-
-  ondropactivate: function (event) {
-    // add active dropzone feedback
-    event.target.classList.add('drop-active');
-    // console.log(event.target)
-  },
-  ondragenter: function (event) {
-    var draggableElement = event.relatedTarget,
-        dropzoneElement = event.target;
-
-    // feedback the possibility of a drop
-    dropzoneElement.classList.add('drop-target');
-    draggableElement.classList.add('can-drop');
-    draggableElement.textContent = 'Dragged in';
-  },
-  ondrop: function (event) {
-    event.relatedTarget.textContent = 'Dropped';
-  },
-  ondropdeactivate: function (event) {
-    // remove active dropzone feedback
-    event.target.classList.remove('drop-active');
-    event.target.classList.remove('drop-target');
-  }
-});
